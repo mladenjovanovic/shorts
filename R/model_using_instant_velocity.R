102631cdc3d869e244b14b1650bed7323ffb3c99
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-04-14 18:41:44 +0200

    Added mixed effects models and fixed few bugs when dealing with NAs

--- R/model_using_instant_velocity.R
+++ R/model_using_instant_velocity.R
@@ -1,12 +1,14 @@
 #' Model Using Instantaneous Velocity
 #'
 #' This function models the sprint instantaneous velocity using mono-exponential equation that estimates
-#'     maximum sprinting speed (\code{MSS}) and relative acceleration (\code{TAU})
+#'     maximum sprinting speed (\code{MSS}) and relative acceleration (\code{TAU}). \code{velocity} is used as target or outcome
+#'     variable, and \code{time} as predictor.
+#'
 #' @param time Numeric vector
 #' @param velocity Numeric vector
 #' @param time_delay Numeric vector. Used to filter out noisy data from the radar gun. Default is 0. See more
 #'     in Samozino (2018)
-#' @param weights Numeric vector. Default is vector of 1s in lengths of \code{time} parameter
+#' @param weights Numeric vector. Default is 1
 #' @param na.rm Logical. Default is FALSE
 #' @return List object with the following elements:
 #'     \describe{
@@ -16,8 +18,8 @@
 #'             \code{RSE}, \code{R_squared}, \code{minErr}, \code{maxErr},
 #'             \code{maxAbsErr}, and \code{RMSE}}
 #'         \item{model}{Model returned by the \code{\link[stats]{nls}} function}
-#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{distance}
-#'             and \code{time} columns}
+#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{time},
+#'             \code{velocity}, \code{time_delay}, \code{weights}, and \code{pred_velocity} columns}
 #'         }
 #' @export
 #' @references
@@ -40,25 +42,28 @@
 model_using_instant_velocity <- function(time,
                                     velocity,
                                     time_delay = 0,
-                                    weights = rep(1, length(time)),
+                                    weights = 1,
                                     na.rm = FALSE) {
 
-  # Remove NAs pairwise
-  if (na.rm) {
-    na.idx <- is.na(time) | is.na(velocity)
-    time <- time[!na.idx]
-    velocity <- velocity[!na.idx]
-  }
-
   # Put data into data frame
-  df <- data.frame(time = time, velocity = velocity, time_delay = time_delay)
+  df <- data.frame(
+    time = time,
+    velocity = velocity,
+    time_delay = time_delay,
+    weights = weights
+    )
+
+  # Remove NAs
+  if (na.rm) {
+    df <- stats::na.omit(df)
+  }
 
   # Non-linear model
   speed_mod <- stats::nls(
     velocity ~ MSS * (1 - exp(1)^(-(time - time_delay)/TAU)),
     data = df,
     start = list(MSS = 7, TAU = 0.8),
-    weights = weights
+    weights = df$weights
   )
 
   # Maximal Sprinting Speed
@@ -72,12 +77,17 @@ model_using_instant_velocity <- function(time,
   PMAX <- (MSS * MAC) / 4
 
   # Model fit
+  pred_velocity <- MSS * (1 - exp(1)^(-(df$time - df$time_delay)/TAU))
+
   RSE <- summary(speed_mod)$sigma
-  R_squared <- stats::cor(time, stats::fitted(speed_mod))^2
-  minErr <- min(stats::fitted(speed_mod) - velocity)
-  maxErr <- max(stats::fitted(speed_mod) - velocity)
-  maxAbsErr <- max(abs(stats::fitted(speed_mod) - velocity))
-  RMSE <- sqrt(mean((stats::fitted(speed_mod) - velocity)^2))
+  R_squared <- stats::cor(df$velocity, pred_velocity)^2
+  minErr <- min(pred_velocity - df$velocity)
+  maxErr <- max(pred_velocity - df$velocity)
+  maxAbsErr <- max(abs(pred_velocity - df$velocity))
+  RMSE <- sqrt(mean((pred_velocity - df$velocity)^2))
+
+  # Add predicted velocity to df
+  df$pred_velocity <- pred_velocity
 
   # Return object
   return(list(
@@ -95,6 +105,123 @@ model_using_instant_velocity <- function(time,
       RMSE = RMSE
     ),
     model = speed_mod,
+    data = df
+  ))
+}
+
+
+#' Mixed Model Using Instantaneous Velocity
+#'
+#' This function models the sprint instantaneous velocity using mono-exponential equation and non-linear
+#'     mixed model using \code{\link[nlme]{nlme}} to estimate fixed and random maximum sprinting speed (\code{MSS})
+#'     and relative acceleration (\code{TAU}) parameters. In mixed model, fixed and random effects are estimated for
+#'     \code{MSS} and \code{TAU} parameters using \code{athlete} as levels. \code{velocity} is used as target or outcome
+#'     variable, and \code{time} as predictor.
+#' @param data Data frame
+#' @param time Character string. Name of the column in \code{data}
+#' @param velocity Character string. Name of the column in \code{data}
+#' @param athlete Character string. Name of the column in \code{data}. Used as levels in the \code{\link[nlme]{nlme}}
+#' @param time_delay Character string. Name of the column in \code{data}. Used to filter out noisy data from the
+#'     radar gun. Default is 0. See more in Samozino (2018)
+#' @param na.rm Logical. Default is FALSE
+#' @return List object with the following elements:
+#'     \describe{
+#'         \item{parameters}{List with two data frames: \code{fixed} and \code{random} containing the following
+#'             estimated parameters: \code{MSS}, \code{TAU}, \code{MAC}, and \code{PMAX}}
+#'         \item{model_fit}{List with the following components:
+#'             \code{RSE}, \code{R_squared}, \code{minErr}, \code{maxErr},
+#'             \code{maxAbsErr}, and \code{RMSE}}
+#'         \item{model}{Model returned by the \code{\link[nlme]{nlme}} function}
+#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{athlete}, \code{time},
+#'           \code{velocity}, \code{time_delay}, and \code{pred_velocity} columns}
+#'         }
+#' @export
+#' @references
+#'     Samozino P. 2018. A Simple Method for Measuring Force, Velocity and Power Capabilities and Mechanical
+#'         Effectiveness During Sprint Running. In: Morin J-B, Samozino P eds. Biomechanics of Training and Testing.
+#'        Cham: Springer International Publishing, 237–267. DOI: 10.1007/978-3-319-05633-3_11.
+#'
+#' @examples
+#' data("radar_gun_data")
+#' mixed_model <- mixed_model_using_instant_velocity(radar_gun_data, "time", "velocity", "athlete")
+#' mixed_model$parameters
+mixed_model_using_instant_velocity <- function(data,
+                                          time,
+                                          velocity,
+                                          athlete,
+                                          time_delay,
+                                          # weights = rep(1, nrow(data)),
+                                          na.rm = FALSE) {
+
+   # Combine to DF
+  df <- data.frame(
+    athlete = data[[athlete]],
+    time = data[[time]],
+    velocity = data[[velocity]],
+    time_delay = ifelse(missing(time_delay), rep(0, nrow(data)), data[[time_delay]]) #,
+    #weights = weights
+  )
+
+  # Remove NAs
+  if (na.rm) {
+    df <- stats::na.omit(df)
+  }
+
+  # Create mixed model
+  mixed_model <- nlme::nlme(
+    velocity ~ MSS * (1 - exp(1)^(-(time - time_delay)/TAU)),
+    data = df,
+    fixed = MSS + TAU~1,
+    random = MSS + TAU~1,
+    groups = ~athlete,
+    # weights = ~weights,
+    start = c(MSS=7,TAU=0.8)
+  )
+
+  # Pull estimates
+  fixed_effects <- nlme::fixed.effects(mixed_model)
+  random_effects <- nlme::random.effects(mixed_model)
+
+  # Fixed effects
+  fixed_effects <- data.frame(t(fixed_effects))
+  fixed_effects$MAC <- fixed_effects$MSS / fixed_effects$TAU
+  fixed_effects$PMAX <- (fixed_effects$MSS * fixed_effects$TAU) / 4
+
+
+  random_effects$athlete <- rownames(random_effects)
+  random_effects$MSS <- random_effects$MSS + fixed_effects$MSS
+  random_effects$TAU <- random_effects$TAU + fixed_effects$TAU
+  rownames(random_effects) <- NULL
+  random_effects <- random_effects[c("athlete", "MSS", "TAU")]
+  random_effects$MAC <- random_effects$MSS / random_effects$TAU
+  random_effects$PMAX <- (random_effects$MSS * random_effects$TAU) / 4
+
+  # Model fit
+  pred_velocity <- stats::predict(mixed_model, newdata = df)
+
+  RSE <- summary(mixed_model)$sigma
+  R_squared <- stats::cor(df$velocity, pred_velocity)^2
+  minErr <- min(pred_velocity - df$velocity)
+  maxErr <- max(pred_velocity - df$velocity)
+  maxAbsErr <- max(abs(pred_velocity - df$velocity))
+  RMSE <- sqrt(mean((pred_velocity - df$velocity)^2))
+
+  # Add predicted velocity to df
+  df$pred_velocity <- pred_velocity
+
+  return(list(
+    parameters = list(
+      fixed = fixed_effects,
+      random = random_effects),
+    model_fit = list(
+      RSE = RSE,
+      R_squared = R_squared,
+      minErr = minErr,
+      maxErr = maxErr,
+      maxAbsErr = maxAbsErr,
+      RMSE = RMSE
+    ),
+    model = mixed_model,
     data = df
   ))
 }
--- R/model_using_split_times.R
+++ R/model_using_split_times.R
@@ -1,10 +1,13 @@
 #' Model Using Split Times
 #'
 #' This function models the sprint split times using mono-exponential equation that estimates
-#'     maximum sprinting speed (\code{MSS}) and relative acceleration (\code{TAU})
+#'     maximum sprinting speed (\code{MSS}) and relative acceleration (\code{TAU}). \code{time} is used as target or outcome
+#'     variable, and \code{distance} as predictor.
 #' @param distance Numeric vector
 #' @param time Numeric vector
-#' @param weights Numeric vector. Default is vector of 1s in lengths of \code{distance} parameter
+#' @param weights Numeric vector. Default is vector of 1
+#'     This is used to give more weight to particular observations. For example, use \code{1\\distance} to give
+#'     more weight to observations from shorter distances.
 #' @param na.rm Logical. Default is FALSE
 #' @return List object with the following elements:
 #'     \describe{
@@ -14,8 +17,8 @@
 #'             \code{RSE}, \code{R_squared}, \code{minErr}, \code{maxErr},
 #'             \code{maxAbsErr}, and \code{RMSE}}
 #'         \item{model}{Model returned by the \code{\link[stats]{nls}} function}
-#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{distance}
-#'             and \code{time} columns}
+#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{distance},
+#'             \code{time}, \code{weights} and \code{pred_time} columns}
 #'         }
 #' @export
 #' @examples
@@ -32,25 +35,28 @@
 #' sprint_model$parameters
 model_using_split_times <- function(distance,
                                     time,
-                                    weights = rep(1, length(distance)),
+                                    weights = 1,
                                     na.rm = FALSE) {
 
-  # Remove NAs pairwise
-  if (na.rm) {
-    na.idx <- is.na(distance) | is.na(time)
-    distance <- distance[!na.idx]
-    time <- time[!na.idx]
-  }
 
   # Put data into data frame
-  df <- data.frame(distance = distance, time = time)
+  df <- data.frame(
+    distance = distance,
+    time = time,
+    weights = weights
+    )
+
+  # Remove NAs
+  if (na.rm) {
+    df <- stats::na.omit(df)
+  }
 
   # Non-linear model
   speed_mod <- stats::nls(
     time ~ TAU * I(LambertW::W(-exp(1)^(-distance / (MSS * TAU) - 1))) + distance / MSS + TAU,
     data = df,
     start = list(MSS = 7, TAU = 0.8),
-    weights = weights
+    weights = df$weights
   )
 
   # Maximal Sprinting Speed
@@ -64,12 +70,17 @@ model_using_split_times <- function(distance,
   PMAX <- (MSS * MAC) / 4
 
   # Model fit
+  pred_time <- TAU * I(LambertW::W(-exp(1)^(-df$distance / (MSS * TAU) - 1))) + df$distance / MSS + TAU
+
   RSE <- summary(speed_mod)$sigma
-  R_squared <- stats::cor(time, stats::fitted(speed_mod))^2
-  minErr <- min(stats::fitted(speed_mod) - time)
-  maxErr <- max(stats::fitted(speed_mod) - time)
-  maxAbsErr <- max(abs(stats::fitted(speed_mod) - time))
-  RMSE <- sqrt(mean((stats::fitted(speed_mod) - time)^2))
+  R_squared <- stats::cor(df$time, pred_time)^2
+  minErr <- min(pred_time - df$time)
+  maxErr <- max(pred_time - df$time)
+  maxAbsErr <- max(abs(pred_time - df$time))
+  RMSE <- sqrt(mean((pred_time - df$time)^2))
+
+  # Add predicted time to df
+  df$pred_time <- pred_time
 
   # Return object
   return(list(
@@ -89,4 +100,113 @@ model_using_split_times <- function(distance,
     model = speed_mod,
     data = df
   ))
+}
+
+
+#' Mixed Model Using Split Times
+#'
+#' This function models the sprint split times using mono-exponential equation and non-linear
+#'     mixed model using \code{\link[nlme]{nlme}} to estimate fixed and random maximum sprinting speed (\code{MSS})
+#'     and relative acceleration (\code{TAU}) parameters. In mixed model, fixed and random effects are estimated for
+#'     \code{MSS} and \code{TAU} parameters using \code{athlete} as levels. \code{time} is used as target or outcome
+#'     variable, and \code{distance} as predictor.
+#' @param data Data frame
+#' @param distance Character string. Name of the column in \code{data}
+#' @param time Character string. Name of the column in \code{data}
+#' @param athlete Character string. Name of the column in \code{data}. Used as levels in the \code{\link[nlme]{nlme}}
+#' @param na.rm Logical. Default is FALSE
+#' @return List object with the following elements:
+#'     \describe{
+#'         \item{parameters}{List with two data frames: \code{fixed} and \code{random} containing the following
+#'             estimated parameters: \code{MSS}, \code{TAU}, \code{MAC}, and \code{PMAX}}
+#'         \item{model_fit}{List with the following components:
+#'             \code{RSE}, \code{R_squared}, \code{minErr}, \code{maxErr},
+#'             \code{maxAbsErr}, and \code{RMSE}}
+#'         \item{model}{Model returned by the \code{\link[nlme]{nlme}} function}
+#'         \item{data}{Data frame used to estimate the sprint parameters, consisting of \code{athlete}, \code{distance},
+#'             \code{time}, and  \code{pred_time} columns}
+#'         }
+#' @export
+#' @examples
+#' data("split_times")
+#'
+#' mixed_model <- mixed_model_using_split_times(split_times, "distance", "time", "athlete")
+#' mixed_model$parameters
+mixed_model_using_split_times <- function(data,
+                                          distance,
+                                          time,
+                                          athlete,
+                                    # weights = rep(1, nrow(data)),
+                                    na.rm = FALSE) {
+  # Combine to DF
+  df <- data.frame(
+    athlete = data[[athlete]],
+    distance = data[[distance]],
+    time = data[[time]]#,
+    #weights = weights
+  )
+
+  # Remove NAs
+  if (na.rm) {
+    df <- stats::na.omit(df)
+  }
+
+  # Create mixed model
+  mixed_model <- nlme::nlme(
+    time~TAU*I(LambertW::W(-exp(1)^(-distance/(MSS*TAU)-1))) + distance / MSS + TAU,
+    data = df,
+    fixed = MSS + TAU~1,
+    random = MSS + TAU~1,
+    groups = ~athlete,
+    # weights = ~weights,
+    start = c(MSS=7,TAU=0.8)
+  )
+
+  # Pull estimates
+  fixed_effects <- nlme::fixed.effects(mixed_model)
+  random_effects <- nlme::random.effects(mixed_model)
+
+  # Fixed effects
+  fixed_effects <- data.frame(t(fixed_effects))
+  fixed_effects$MAC <- fixed_effects$MSS / fixed_effects$TAU
+  fixed_effects$PMAX <- (fixed_effects$MSS * fixed_effects$TAU) / 4
+
+
+  random_effects$athlete <- rownames(random_effects)
+  random_effects$MSS <- random_effects$MSS + fixed_effects$MSS
+  random_effects$TAU <- random_effects$TAU + fixed_effects$TAU
+  rownames(random_effects) <- NULL
+  random_effects <- random_effects[c("athlete", "MSS", "TAU")]
+  random_effects$MAC <- random_effects$MSS / random_effects$TAU
+  random_effects$PMAX <- (random_effects$MSS * random_effects$TAU) / 4
+
+   # Model fit
+
+  pred_time <- stats::predict(mixed_model, newdata = df)
+  RSE <- summary(mixed_model)$sigma
+  R_squared <- stats::cor(df$time, pred_time)^2
+  minErr <- min(pred_time - df$time)
+  maxErr <- max(pred_time - df$time)
+  maxAbsErr <- max(abs(pred_time - df$time))
+  RMSE <- sqrt(mean((pred_time -df$time)^2))
+
+  # Add predicted time to df
+  df$pred_time <- pred_time
+
+ return(list(
+   parameters = list(
+     fixed = fixed_effects,
+     random = random_effects),
+   model_fit = list(
+     RSE = RSE,
+     R_squared = R_squared,
+     minErr = minErr,
+     maxErr = maxErr,
+     maxAbsErr = maxAbsErr,
+     RMSE = RMSE
+   ),
+   model = mixed_model,
+   data = df
+ ))
+
 }
