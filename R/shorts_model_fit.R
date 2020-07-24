3a8dcf9b96b9d4a95cd2408859175cb2377e09da
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-07-24 08:45:19 +0200

    Modified function to suit LOOCV

--- R/shorts_model_fit.R
+++ R/shorts_model_fit.R
@@ -1,8 +1,16 @@
-shorts_model_fit <- function(model, observed, predicted, na.rm = FALSE) {
+shorts_model_fit <- function(model = NULL, observed, predicted, na.rm = FALSE) {
   resid <- predicted - observed
 
+  # This is needed for the LOOCV option which doesn't return model
+  # thus, RSE cannot be computed
+  if(is.null(model)) {
+    RSE <- NA
+  } else {
+    RSE = summary(model)$sigma
+  }
+
   list(
-    RSE = summary(model)$sigma,
+    RSE = RSE,
     R_squared = stats::cor(observed, predicted)^2,
     minErr = min(resid, na.rm = na.rm),
     maxErr = max(resid, na.rm = na.rm),
