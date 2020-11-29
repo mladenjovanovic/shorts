517152df21b6f99331c93521a7fc49be1f7d1bc0
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-11-29 19:10:14 +0100

    Plotting and printing for get_fv_profile done

--- R/shorts_constructors.R
+++ R/shorts_constructors.R
@@ -24,3 +24,37 @@ new_shorts_mixed_model <- function(parameters, model_fit, model, data, LOOCV = N
   class(shorts_mixed_model_object) <- "shorts_mixed_model"
   return(shorts_mixed_model_object)
 }
+
+new_fv_profile <- function(bodymass,
+                           F0,
+                           F0_rel,
+                           V0,
+                           Pmax,
+                           Pmax_relative,
+                           FV_slope,
+                           RFmax_cutoff,
+                           RFmax,
+                           Drf,
+                           RSE_FV,
+                           RSE_Drf,
+                           data) {
+  fv_profile <- list(
+    bodymass = bodymass,
+    F0 = F0,
+    F0_rel = F0_rel,
+    V0 = V0,
+    Pmax = Pmax,
+    Pmax_relative = Pmax_relative,
+    FV_slope = FV_slope,
+    RFmax_cutoff = RFmax_cutoff,
+    RFmax = RFmax,
+    Drf = Drf,
+    RSE_FV = RSE_FV,
+    RSE_Drf = RSE_Drf,
+    data = data
+  )
+
+  class(fv_profile) <- "shorts_fv_profile"
+  return(fv_profile)
+
+}
