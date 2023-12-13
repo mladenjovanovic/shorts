5297ee3926492dc9d8d7352e3ac2706574ce89fc
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-12-13 08:29:22 +0100

    Updated data to have some time and distance shift

--- data-raw/dynaspeed.R
+++ data-raw/dynaspeed.R
@@ -2,5 +2,7 @@
 
 # Load data
 dynaspeed <- read.csv("data-raw/dynaspeed.csv", header = TRUE)
+dynaspeed$time <- dynaspeed$time + 1.43
+dynaspeed$distance <- dynaspeed$distance + 2.74
 
 usethis::use_data(dynaspeed, overwrite = TRUE)
