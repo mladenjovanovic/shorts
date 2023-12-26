263dfbd294a041d401a1813cd122e0718b734b47
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-12-26 22:51:47 +0100

    examples

--- dev/examples.R
+++ dev/examples.R
@@ -1,9 +1,13 @@
-# Model Timing Gates (with Flying Distance Correction and Time Correction)
-df <- create_sprint_trace(MSS = 8, MAC = 6, distance = c(5, 10, 15, 20, 30, 40), TC = 0.2, FD = 0.5)
-m1 <- model_timing_gates_FD_TC(
-  distance = df$distance,
-  time = df$time,
-  # Control forwarded to minpack.lm::nlsLM()
-  control = minpack.lm::nls.lm.control(maxiter = 1000))
-m1
-plot(m1)
+split_distances <- c(10, 20, 30, 40, 50)
+split_times <- create_timing_gates_splits(
+  gates = split_distances,
+  MSS = 10,
+  MAC = 9,
+  FD = 0,
+  TC = 0,
+  noise = 0.01
+)
+#'
+# Simple model
+simple_model <- model_timing_gates(split_distances, split_times)
+confint(simple_model)
