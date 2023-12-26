4de276298b1ba838378ed6547f56830df8a7f01a
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-12-26 22:13:28 +0100

    Copied from the "master" so I can work on the examples

--- R/create-sprint-trace.R
+++ R/create-sprint-trace.R
@@ -1,6 +1,6 @@
 #' Create Sprint Trace
 #'
-#' This function creates sprint trace either using `time` or `distance` vectors
+#' This function creates sprint trace either using \code{time} or \code{distance} vectors
 #' @param MSS,MAC  Numeric vector. Model parameters
 #' @param time Numeric vector.
 #' @param distance Numeric vector.
@@ -9,8 +9,8 @@
 #' @param FD Numeric vector. Flying start distance. Default is 0
 #' @return Data-frame with following 6 columns
 #'     \describe{
-#'        \item{time}{Measurement-scale time vector in seconds}
-#'        \item{distance}{Measurement-scale distance vector in meters}
+#'        \item{time}{Measurement-scale time vector in seconds. Equal to parameter \code{time}}
+#'        \item{distance}{Measurement-scale distance vector in meters. Equal to parameter \code{distance}}
 #'        \item{velocity}{Velocity vector in m/s}
 #'        \item{acceleration}{Acceleration vector in m/s/s}
 #'        \item{sprint_time}{Sprint scale time vector in seconds. Sprint always start at t=0s}
@@ -18,8 +18,8 @@
 #'     }
 #' @export
 #' @examples
-#' df <- create_sprint_trace(8, 7, time = seq(0, 6, by = 0.01))
 #'
+#' df <- create_sprint_trace(8, 7, time = seq(0, 6, by = 0.01))
 #' df <- create_sprint_trace(8, 7, distance = seq(0, 40, by = 1))
 #'
 #' @export
@@ -34,23 +34,37 @@ create_sprint_trace <- function(MSS,
     stop("Please use either time or distance vector, not both.", call. = FALSE)
   }
 
+  if (FD < 0) {
+    stop("Flying start distance (FD) cannot be negative number.", call. = FALSE)
+  }
+
   # TRUE sprint performance data, with the start at d=0 and t=0
+  FD_time <- predict_time_at_distance(FD, MSS, MAC)
+
   if (!is.null(time)) {
-    df <- data.frame(sprint_time = time)
+    df <- data.frame(time = time)
+    df$sprint_time <- df$time + FD_time - TC
+    df$sprint_time <- ifelse(df$sprint_time < 0, 0, df$sprint_time)
 
     df$sprint_distance <- predict_distance_at_time(
       time = df$sprint_time,
       MSS = MSS,
       MAC = MAC
     )
+
+    df$distance <- df$sprint_distance + DC - FD
   } else if (!is.null(distance)) {
-    df <- data.frame(sprint_distance = distance)
+    df <- data.frame(distance = distance)
+    df$sprint_distance <- df$distance + FD - DC
+    df$sprint_distance <- ifelse(df$sprint_distance < 0, 0, df$sprint_distance)
 
     df$sprint_time <- predict_time_at_distance(
       distance = df$sprint_distance,
       MSS = MSS,
       MAC = MAC
     )
+
+    df$time <- df$sprint_time + TC - FD_time
   }
 
   # Add velocity and acceleration
@@ -60,23 +74,15 @@ create_sprint_trace <- function(MSS,
     MAC = MAC
   )
 
+  df$velocity <- ifelse(df$velocity < 0, 0, df$velocity)
+
   df$acceleration <- predict_acceleration_at_time(
     time = df$sprint_time,
     MSS = MSS,
     MAC = MAC
   )
 
-  # Filter-out everything below flying-distance FD since this is not seen
-  # by the measurement device
-  df <- df[df$sprint_distance >= FD, ]
-
-  # Now, set this to be t=0 and d=0
-  df$time <- df$sprint_time - df$sprint_time[1]
-  df$distance <- df$sprint_distance - df$sprint_distance[1]
-
-  # Now add TC and DC
-  df$time <- df$time + TC
-  df$distance <- df$distance + DC
+  df$acceleration <- ifelse(df$time < TC, 0, df$acceleration)
 
   data.frame(
     time = df$time,
